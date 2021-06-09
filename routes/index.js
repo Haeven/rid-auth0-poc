5690668bf9d7dd51f8d8436fcf9210eef8c411bb
haevenkelley <hkelley@ritchiebros.com>
2021-06-09 17:34:01 -0500

    make it better

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+// router.get('/logins', function (req, res, next) {
+	// const options = { method: 'POST',
+	// 	url: 'https://dev-ivdsn1wz.us.auth0.com/oauth/token',
+	// 	headers: { 'content-type': 'application/json' },
+	// 	body: '{"client_id":"SBZsADpFTJleFDn8CUOeX1rVXJg7zFow","client_secret":"BI-fDkgmKj4QeHP8YV62EnDpoYQBpMHiFMyFCBh7zQjQrr2B-abmrOd6ds_7n3Hr","audience":"https://dev-ivdsn1wz.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
+	// };
+	// console.log(req); /* eslint-disable-line */
+  // res.status(200);
+  // res.status(200).json({
+	// 	user_id: '01jd383hd',
+	// 	nickname: 'Haeven',
+	// 	email: 'haeven@gmail.com'
+	// });
+  // res.render('index', {
+  //   title: 'Auth0 Webapp sample Nodejs',
+  //   isAuthenticated: true
+  // });
+// });
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,87 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const https = require("https");
+const logger = require("morgan");
+const path = require("path");
+const router = require("./routes/index");
+const { auth } = require("express-openid-connect");
+
+dotenv.load();
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+};
+
+const port = process.env.PORT || 3000;
+if (
+  !config.baseURL &&
+  !process.env.BASE_URL &&
+  process.env.PORT &&
+  process.env.NODE_ENV !== "production"
+) {
+  config.baseURL = `https://rbauction.local:8443`;
+}
+
+app.use(auth(config));
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
+
+app.use("/", router);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// var server = https.createServer({
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: certificateAuthority,
+// 	ciphers: [
+// 			"ECDHE-RSA-AES128-SHA256",
+// 			"DHE-RSA-AES128-SHA256",
+// 			"AES128-GCM-SHA256",
+// 			"RC4",
+// 			"HIGH",
+// 			"!MD5",
+// 			"!aNULL"
+// 	].join(':'),
+// }, app);
+
+// Error handlers
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: process.env.NODE_ENV !== "production" ? err : {},
+  });
+});
+const fs = require("fs");
+
+const privateKey = fs.readFileSync("certs/server.key", "utf8");
+const certificate = fs.readFileSync("certs/server.crt", "utf8");
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
+https.createServer(credentials, app).listen(port, () => {
+  console.log(`Listening on ${config.baseURL}`);
+});
