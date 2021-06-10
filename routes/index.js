485d2402179487d425ba9edcfec12cadb2fa8654
haevenkelley <hkelley@ritchiebros.com>
2021-06-09 19:13:06 -0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ router.get('/auth0/login', function (req, res, next) {
 		body: '{"client_id":"SBZsADpFTJleFDn8CUOeX1rVXJg7zFow","client_secret":"BI-fDkgmKj4QeHP8YV62EnDpoYQBpMHiFMyFCBh7zQjQrr2B-abmrOd6ds_7n3Hr","audience":"https://dev-ivdsn1wz.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
 	};
 
-console.log(req.username); /* eslint-disable-line */
-console.log(req.password); /* eslint-disable-line */
+
+console.log((req.auth) ? req.auth.username : ''); /* eslint-disable-line */
   res.status(200).json({
 		user_id: '01jd383hd',
 		nickname: 'Haeven',
