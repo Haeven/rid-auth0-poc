9f0743b8bf7fe8a9a4153fd20683546f689cc3ce
haevenkelley <hkelley@ritchiebros.com>
2021-06-09 19:07:31 -0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -9,24 +9,25 @@ router.get('/', function (req, res, next) {
 	res.end();
 });
 
-// router.get('/logins', function (req, res, next) {
-	// const options = { method: 'POST',
-	// 	url: 'https://dev-ivdsn1wz.us.auth0.com/oauth/token',
-	// 	headers: { 'content-type': 'application/json' },
-	// 	body: '{"client_id":"SBZsADpFTJleFDn8CUOeX1rVXJg7zFow","client_secret":"BI-fDkgmKj4QeHP8YV62EnDpoYQBpMHiFMyFCBh7zQjQrr2B-abmrOd6ds_7n3Hr","audience":"https://dev-ivdsn1wz.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
-	// };
-	// console.log(req); /* eslint-disable-line */
-  // res.status(200);
-  // res.status(200).json({
-	// 	user_id: '01jd383hd',
-	// 	nickname: 'Haeven',
-	// 	email: 'haeven@gmail.com'
-	// });
+router.get('/auth0/login', function (req, res, next) {
+	const options = {
+		method: 'POST',
+		url: 'https://dev-ivdsn1wz.us.auth0.com/oauth/token',
+		headers: { 'content-type': 'application/json' },
+		body: '{"client_id":"SBZsADpFTJleFDn8CUOeX1rVXJg7zFow","client_secret":"BI-fDkgmKj4QeHP8YV62EnDpoYQBpMHiFMyFCBh7zQjQrr2B-abmrOd6ds_7n3Hr","audience":"https://dev-ivdsn1wz.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
+	};
+
+
+  res.status(200).json({
+		user_id: '01jd383hd',
+		nickname: 'Haeven',
+		email: 'haeven@gmail.com'
+	});
   // res.render('index', {
   //   title: 'Auth0 Webapp sample Nodejs',
   //   isAuthenticated: true
   // });
-// });
+});
 
 router.get('/profile', function (req, res, next) {
   res.render('profile', {
